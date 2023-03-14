64db587cce8c37315aadebbafa3ce1a54b227a22
shasherazi <hassanrandomz@gmail.com>
2023-03-14 15:43:59 +0500

    Add conditional back chevron

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { IoChevronBack } from 'react-icons/io5';
 import styles from '../styles/Navbar.module.css';
 
-const Navbar = () => (
-  <div className={styles.navbar}>
-    <h1>My Blog</h1>
-    <Outlet />
-  </div>
-);
+const Navbar = () => {
+  const location = useLocation();
+  return (
+    <div className={styles.navbar}>
+      <NavLink to="/" className={styles.link}>
+        { location.pathname !== '/' && <IoChevronBack className={styles.back} /> }
+        {/* <IoChevronBack className={styles.back} /> */}
+      </NavLink>
+      <Outlet />
+    </div>
+  );
+};
+
 export default Navbar;
